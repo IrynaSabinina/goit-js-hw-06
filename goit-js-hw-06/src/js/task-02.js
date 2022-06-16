507f86479e1e107c3c920af5fcd18f6ba58c4f30
Iryna <pysarenkoiryna@gmail.com>
2022-06-16 10:24:28 +0300

    hw3

--- goit-js-hw-06/src/js/task-02.js
+++ goit-js-hw-06/src/js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listElement = document.querySelector("#ingredients");
+
+const itemsList = ingredients.map(function (ingredient) {
+  const list = document.createElement("li");
+  list.textContent = ingredient;
+  return list;
+});
+listElement.append(...itemsList);
