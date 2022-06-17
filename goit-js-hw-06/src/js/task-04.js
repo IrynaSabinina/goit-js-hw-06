63aed9e049854bb287e308546486a530bfa22793
Iryna <pysarenkoiryna@gmail.com>
2022-06-17 15:40:40 +0300

    Update04

--- goit-js-hw-06/src/js/task-04.js
+++ goit-js-hw-06/src/js/task-04.js
@@ -1,1 +1,16 @@
-let counterValue = 0;
+const counterValue = document.querySelector("#value");
+let counterFirst = 0;
+
+const counter = document.querySelector("#counter");
+const leftBtn = counter.firstElementChild;
+leftBtn.addEventListener("click", () => {
+  counterFirst -= 1;
+  return (counterValue.textContent = counterFirst);
+});
+
+console.log(leftBtn);
+const rightBtn = counter.lastElementChild;
+rightBtn.addEventListener("click", () => {
+  counterFirst += 1;
+  return (counterValue.textContent = counterFirst);
+});
